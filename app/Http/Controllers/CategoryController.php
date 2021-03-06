06dbef00db5727bc1820682cdda24b4e2f7fa08d
azaharul-just <47347012+azaharul-just@users.noreply.github.com>
2021-03-06 08:51:21 +0600

    first commit all file added

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Auth;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class CategoryController extends Controller
+{
+    public function AllCat(){
+        //$categories = Category::latest()->get(); //Elequent: all data from category model table
+        //$categories = Category::latest()->paginate(10);  //With Pagination
+        $categories = DB::table('categories')->latest()->paginate(5); //Query Builder
+        return view('admin.category.index',compact('categories'));
+    }
+
+    public function AddCat(Request $request){
+        $validatedData = $request->validate([
+            'category_name' => 'required|unique:categories|max:255', 
+        ],[
+            'category_name.required' => 'Please Fill Up Category Name', 
+            'category_name.max' => 'Category Name less than 255 Chars', 
+        ]); 
+
+        //Eloquent ORM
+
+        // Category::insert([
+        //     'category_name' => $request->category_name,
+        //     'user_id'   =>Auth::user()->id,
+        //     'created_at'=>Carbon::now()
+        // ]);
+
+        //Use these or above as you wise but these prefered professionally
+
+        $category = new Category; //Model Class object
+        $category->category_name = $request->category_name;
+        $category->user_id = Auth::user()->id; //Current user id setup
+        $category->save();
+
+        //Query Builder
+
+        // $data = array();
+        // $data['category_name'] = $request->category_name;
+        // $data['user_id']= Auth::user()->id;
+        // DB::table('categories')->insert($data);
+
+        return redirect()->back()->with('success','Category Added Successfully');
+
+
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\CategoryController;
+use App\Models\User;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/contactsdgfhg', [ContactController::class, 'index'])->name('contact');
+
+
+//CategoryController
+Route::get('/category/all', [CategoryController::class, 'AllCat'])->name('all.category');
+Route::post('/category/add', [CategoryController::class, 'AddCat'])->name('store.category');
+
+
+ 
+ 
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+
+    $users=User::all();
+    return view('dashboard',compact('users'));
+})->name('dashboard');

97693690e56dfc4cca41f43865ecce81660db0a2
azaharul-just <47347012+azaharul-just@users.noreply.github.com>
2021-03-07 11:03:37 +0600

    brand image created successfuylly

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BrandController;
 use App\Models\User;
 /*
 |--------------------------------------------------------------------------
@@ -41,9 +42,9 @@ Route::get('/softdelete/category/{id}', [CategoryController::class, 'SoftDelete'
 Route::get('/category/restore/{id}', [CategoryController::class, 'Restore']);
 Route::get('/pdelete/category/{id}', [CategoryController::class, 'PDelete']);
 
-
-
-
+//Brand Controller 
+Route::get('/brand/all/', [BrandController::class, 'AllBrand'])->name('all.brand');
+Route::post('/brand/add', [BrandController::class, 'AddBrand'])->name('store.brand');
 
 
 
